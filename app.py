094157b096f9260a6d7758516e7502b08c7ef191
MoonKd <90130170+MoonKd@users.noreply.github.com>
2021-09-11 16:22:48 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
-from pymongo import MongoClient
-
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.dbhomework
 
-
-# HTML 화면 보여주기
 @app.route('/')
 def home():
-    return render_template('test.html')
-
-
-# API 역할을 하는 부분
-@app.route('/api/list', methods=['GET'])
-def show_stars():
-    pop = list(db.rests1.find({}, {'_id': False}))
-    return jsonify({'pop_star': pop})
-
-
+   return render_templates('indexnew.html')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0',port=5000,debug=True)
