83d0cc04304cc8bd19648be6b1db51fd9845a206
MoonKd <90130170+MoonKd@users.noreply.github.com>
2021-09-12 16:05:04 +0900

    updated link function

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template, jsonify, request
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
-from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-db = client.dbhomework
 
 @app.route('/')
 def home():
-   return render_templates('indexnew.html')
+   return render_template('indexnew.html')
 
+@app.route('/boardnew.html')
+def recommend():
+   return render_template('boardnew.html')
+
+@app.route('/indexnew.html')
+def indexnew():
+   return render_template('indexnew.html')
+
+@app.route('/introduce_member.html')
+def introduce():
+   return render_template('introduce_member.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
