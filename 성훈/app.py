efff49c33f486e8e26b5511dec1e68e3defa924c
김성훈 <90130141+sunghoonking@users.noreply.github.com>
2021-09-15 15:28:31 +0900

    Add files via upload
    제안하기,리뷰하기 app.py

--- 성훈/app.py
+++ 성훈/app.py
@@ -5,17 +5,36 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-# HTML 화면 보여주기
+## HTML을 주는 부분
 @app.route('/')
 def home():
-    return render_template('indexnew.html')
+    return render_template('456.html')
 
-# API 역할을 하는 부분
-@app.route('/api/list', methods=['GET'])
-def show_stars():
-    pop = list(db.lats1.find({}, {'_id': False}))
-    return jsonify({'pop_star': pop})
+@app.route('/review', methods=['POST'])
+def write_review():
+    # title_receive로 클라이언트가 준 title 가져오기
+    title_receive = request.form['title_give']
+    # author_receive로 클라이언트가 준 author 가져오기
+    author_receive = request.form['author_give']
+    # review_receive로 클라이언트가 준 review 가져오기
+    review_receive = request.form['review_give']
 
+    # DB에 삽입할 review 만들기
+    doc = {
+        'title': title_receive,
+        'author': author_receive,
+        'review': review_receive
+    }
+    # reviews에 review 저장하기
+    db.bookreview.insert_one(doc)
+    # 성공 여부 & 성공 메시지 반환
+    return jsonify({'msg': '리뷰가 성공적으로 작성되었습니다.'})
+
+@app.route('/review', methods=['GET'])
+def read_reviews():
+    reviews = list(db.bookreview.find({}, {'_id': False}))
+
+    return jsonify({'all_reviews': reviews})
 
 
 
